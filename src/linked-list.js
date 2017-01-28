d9822a6d9ad7cf87ffa0dcd6d49a379711e7e091
Tatsiana Zare <danee4ka2@mail.ru>
2017-01-28 16:51:08 +0300

    Add append method

--- src/linked-list.js
+++ src/linked-list.js
@@ -3,9 +3,16 @@ const Node = require('./node');
 class LinkedList {
     constructor() {
         this.length = 0;
+        this._head = null;
+        this._tail = null;
     }
 
-    append(data) {}
+    append(data) {
+        const node = new Node(data);
+        this._head = node;
+        this._tail = node;
+        
+    }
 
     head() {}
 

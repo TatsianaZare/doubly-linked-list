03efa4b79455d408ded83b6b0a2ea3cccc1c5395
Tatsiana Zare <danee4ka2@mail.ru>
2017-01-28 18:20:12 +0300

    Add head ant tail

--- src/linked-list.js
+++ src/linked-list.js
@@ -8,15 +8,29 @@ class LinkedList {
     }
 
     append(data) {
-        const node = new Node(data);
-        this._head = node;
-        this._tail = node;
-        
+        // For empty list
+        if (this._tail == null) {
+            const node = new Node(data);
+            this._head = node;
+            this._tail = node;
+        } else {
+            const node = new Node(data);
+            node.prev = this._tail;
+            this._tail.next = node;
+            this._tail = node;
+        }
+        this.length += 1;
     }
 
-    head() {}
+    head() {
+        const node = this._head;
+        return node.data;
+    }
 
-    tail() {}
+    tail() {
+        const node = this._tail;
+        return node.data;
+    }
 
     at(index) {}
 
